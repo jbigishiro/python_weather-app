e4793b119e81839168e42449636bc6e4872c699e
justin <justin.rvd@gmail.com>
2024-01-27 04:44:23 -0600

    implement the sky data and images

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
 import plotly.express as px
-from backend import foo
-
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place")
@@ -10,18 +8,33 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+if days > 1:
+    d = "days"
+else:
+    d = "day"
 
+if place:
+    st.subheader(f"{option} for the next {days} {d} in {place}")
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+    filtered_data = get_data(place=place, forecasted_days=days)
+    dates = [dict["dt_txt"] for dict in filtered_data]
 
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        temperatures_in_F = [round((k - 273.15) * 9 / 5 + 32, 2) for k in temperatures]
 
-y_data = foo(place, days, option)
+        figure = px.scatter(x=dates, y=temperatures_in_F, labels={"x": "Time", "y": "Temperature"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+    elif option == "Sky":
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
 
-st.plotly_chart(figure)
+        images = {'Rain': "sky_images/rain.png", 'Clouds': "sky_images/cloud.png",
+                  'Clear': "sky_images/clear.png", 'Snow': "sky_images/snow.png"}
+        image_paths = [images[condition] for condition in sky_conditions]
+
+        # figure = px.scatter(x=dates, y=sky_conditions, labels={"x": "Time", "y": "Sky"})
+        st.image(image_paths, width=115)
+
+else:
+    st.subheader("Select the place")
